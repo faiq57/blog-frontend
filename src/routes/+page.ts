b8c734629a53814ef779a0e53fbc1e8b36ee7a73
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-08-01 00:16:29 +1000

    fix: Made the website not crash if backend is down

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,23 +5,32 @@ export const load = async () => {
 
     const response = await fetch(request);
     if (!response.ok) {
-        return {};
+        return { articles: [] };
     };
 
     const articles = await response.json();
 
+    const commentPromises = []
     for (const article of articles) {
         const request = new Request("https://blog-data.phaeiq.com/comments/" + article.id.toString(), {
             method: "GET",
         });
 
-        const response = await fetch(request)
-        if (!response.ok) {
-            article.comments = [];
-        } else {
-            article.comments = await response.json()
-        }
+        commentPromises.push(fetch(request)
+            .then((response) => {
+                if (!response.ok) {
+                    return []
+                } else {
+                    return response.json()
+                }
+            })
+            .then((commentArr) => {
+                article.comments = commentArr;
+            })
+        )
     }
+
+    await Promise.all(commentPromises);
 
     return { articles: articles };
 }
