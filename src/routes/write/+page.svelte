d983cc2f8382ba9fe9969fe0f2ccee22de86e976
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-08-06 21:15:49 +1000

    feat: Started working on the blog post writing page

---
+++ src/routes/write/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import MainBanner from "$lib/components/MainBanner.svelte";
+    import SidebarButton from "./SidebarButton.svelte";
+</script>
+
+<MainBanner />
+<div class="flex items-start h-full w-full border-t-4 border-t-[#ffaaff] bg-[#ffaaff] text-[#9f0f33]">
+    <div class="flex flex-col bg-[#ffaaff] h-full min-h-dvh w-20">
+        <SidebarButton>
+            Add text
+        </SidebarButton>
+        <SidebarButton>
+            Add a pic
+        </SidebarButton>
+    </div>
+</div>
---
+++ src/routes/write/SidebarButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { onclick, children } = $props()
+</script>
+
+<button {onclick} class="bg-[#f109f1] m-1 p-2 rounded-sm text-white">
+    {@render children()}
+</button>
