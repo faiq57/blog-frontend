a7658cec9c73f0e4b5270d71bcefb9f1b1fb4c4d
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-08-06 16:12:03 +1000

    chore: Extracted out the blog posts into a component and added a page title for browsers

---
+++ src/lib/components/BlogPost.svelte
@@ -0,0 +1,134 @@
+<script lang='ts'>
+    import { invalidate } from "$app/navigation";
+
+    let { article } = $props();
+    let commentAuthor = $state("");
+    let commentBody = $state("")
+
+    const addNewComment = (author: String, content: String, postId: Number) => {
+        const request = new Request("https://blog-data.phaeiq.com/comments", {
+            method: "POST",
+            body: JSON.stringify({
+                'author': author,
+                'content': content,
+                'postId': postId
+            })
+        })
+        commentAuthor = "";
+        commentBody = "";
+        return fetch(request)
+    }
+
+    const submitNewComment = (postId: Number) => {
+        const postIdString = postId.toString();
+        addNewComment(commentAuthor, commentBody, postId)
+            .then((res) => {
+                if (res.ok) {
+                    invalidate("https://blog-data.phaeiq.com/comments/" + postId.toString())
+                }
+            })
+    }
+</script>
+
+<div class="mx-2 text-[#9f0f33]">
+    <h1 class="font-bold text-2xl text-center mb-1">
+        {article.title}
+    </h1>
+    <div class="flex">
+        <div class="font-bold">By {article.author}</div>
+        <div class="font-bold ml-auto">{(new Date(article.timestamp + "Z")).toLocaleString()}</div>
+    </div>
+    <div class="article-content mb-8 mt-2 text-justify">
+        {@html article.content}
+        <div class="bg-[#ffaaff] p-2 text-[#a01020] rounded-sm">
+            <h2 class="font-bold">Comments</h2>
+            <div class="bg-[#afccf0] my-1 p-2 flex flex-col">
+                <label for={article.id + "-comment-box"} class="mb-2">Say something!</label> 
+                <textarea
+                    bind:value={commentBody}
+                    id={article.id + "-comment-box"}
+                    class="bg-amber-200 p-2"
+                    placeholder="Comment here!"
+                    autocomplete="off"
+                    ></textarea>
+                <div class="flex mt-2">
+                    <label for={article.id + "-comment-author"} class="p-2">From:</label>
+                    <input
+                        autocomplete="off"
+                        bind:value={commentAuthor}
+                        id={article.id + "-comment-author"}
+                        class="bg-amber-200 p-2 max-w-[20dvw]"
+                        placeholder="Name"/>
+                    <button 
+                        class="px-2 ml-auto cursor-pointer bg-[#ffccff] rounded-2xl"
+                        type="button"
+                        onclick={() => submitNewComment(article.id)}
+                        >Submit comment</button>
+                </div>
+            </div>
+            {#each article.comments as comment}
+                <div class="bg-[#ffccff] my-1 p-2 flex flex-col">
+                    <div class="font-bold flex">
+                        <div>{comment.author}</div>
+                        <div class="ml-auto">
+                            {(new Date(comment.timestamp + "Z")).toLocaleString()}
+                        </div>
+                    </div>
+                    {comment.content} 
+                </div>
+            {/each}
+        </div>
+    </div>
+</div>
+
+<style>
+    .article-content :global(p) {
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
+
+    .article-content :global(img) {
+        max-width: 100%;
+        height: auto;
+        margin-inline: auto;
+    }
+    
+    .article-content :global(.vertical-image) {
+        max-height: 500px;
+        max-width: 70dvw;
+        height: auto;
+        width: auto;
+    }
+
+    .article-content :global(.hover-bright):hover {
+        filter: brightness(5);
+    }
+
+    .article-content :global(.hover-sepia):hover {
+        filter: sepia(0.7)
+    }
+
+    .article-content :global(.hover-invert):hover {
+        filter: invert(1);
+    }
+
+   .article-content :global(.hover-magenta):hover {
+        filter: hue-rotate(-40deg);
+    }
+
+    .article-content :global(.hover-desaturate):hover {
+        filter: saturate(0.5);
+    }
+
+    .article-content :global(.top-image) {
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        border-bottom: 1px solid white;
+    }
+
+    .article-content :global(.bottom-image) {
+        border-end-end-radius: 10px;
+        border-bottom-left-radius: 10px;
+        border-top: 1px solid white;
+    }
+</style>
